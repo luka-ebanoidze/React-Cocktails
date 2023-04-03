45781bc239301d28eab3cda3e182bc1989886e35
luka-ebanoidze <ebanoidzelukaa@gmail.com>
2023-04-03 12:43:25 +0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
+        nesciunt commodi magni, animi ducimus, fugit numquam necessitatibus
+        autem voluptate corporis nemo blanditiis esse soluta quos, eius
+        molestias deleniti ad sequi. Minima, reprehenderit eveniet doloremque
+        vero modi nisi officiis? Nemo, minima.
+      </p>
+    </section>
+  );
+};
+
+export default About;
